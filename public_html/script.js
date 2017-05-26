6889c63b59f01ceaad6683f96c340815c93853e4
Anderson Felipe <andersonfseabra3@gmail.com>
2017-05-26 11:41:52 -0300

    adicionado esboço dos movimentos e adicionado as guias

--- public_html/script.js
+++ public_html/script.js
@@ -1,49 +1,77 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-//$(document).ready(function(){
+    var toggle = true;
     $(function(){
-          $('.drag').draggable({revert:'invalid'},{scope:"cacete"});
-            montaGrid();
-             $('.tab').droppable({scope:'cacete'},{revert:'fit'});
-       
-        
-    });
-//});
 
-function montaGrid(){
-    var qntCasas = 70;
-    var id_auto = 1;
-    var controle = 0;
-    var cor =  0;
-    var a = new Array('a','b','c','d','e','f','g','h');
-for(var i = 0; i <= qntCasas; i++){
-    
-      if(id_auto == 9){
-          id_auto = 1;
-          controle ++;
-          a[controle];
-          if(cor == 0){
+          mostrarGuia();
+          montaGrid();
+          $('.drag').draggable({revert:'invalid'},{scope:'cacete'});
+          $('.tab').droppable({scope:'cacete'},{revert:'fit'},{accept: '.drag'});
+          $('.tab').droppable({ drop: function( event, ui ) { 
+
+              //alert('Olá Você está em -> '+$(this).attr('id'));
+
+            }
+          });
+    });
+
+
+    function montaGrid(){
+      var qntCasas = 70;
+      var id_auto = 1;
+      var controle = 0;
+      var cor =  0;
+      var a = new Array('A','B','C','D','E','F','G','H');
+      for(var i = 0; i <= qntCasas; i++){
+      
+        if(id_auto == 9){
+
+            id_auto = 1;
+            controle ++;
+            a[controle];
+
+            if(cor == 0){
               cor = 1;
-          }else {
+            }else {
               cor = 0;
-          }
-      }else{
+            }
+
+        }else{
           if(cor == 0){
-                  var elemento = $('#tabuleiro').append("<div id="+a[controle]+id_auto+" class='tab' style='width:80px; height:80px; float:left; background-color:white; border: 1px solid black;'></div>");
-                  id_auto ++;      
-                cor = 1;
+            var elemento = $('#tabuleiro').append("<div id="+a[controle]+id_auto+" class='tab' style='width:80px; height:80px; float:left; background-color:white; border: 1px solid black;'><p>"+a[controle]+id_auto+"</p></div>");
+            id_auto ++;      
+            cor = 1;
           }else{
-                var elemento = $('#tabuleiro').append("<div id="+a[controle]+id_auto+" class='tab' style='width:80px; height:80px; float:left; background-color:black; border: 1px solid white;'></div>");
-                 id_auto ++;
-                 cor = 0;
-             }
+            var elemento = $('#tabuleiro').append("<div id="+a[controle]+id_auto+" class='tab' style='width:80px; height:80px; float:left; background-color:black; border: 1px solid black;'><p>"+a[controle]+id_auto+"</p></div>");
+            id_auto ++;
+            cor = 0;
+          }
         }
-  }
-}
+      }
+    }
+
+
+
+    function mostrarGuia(){
+      $('button').click(function(event) {
+        if(toggle == true){
+          toggle  = false;
+        } else {
+          toggle = true;
+        }
+
+        if(toggle == true){
+            $('p').show();
+            $('button').html('Esconder Guias');
+        } else {
+          $('p').hide();
+            $('button').html('Mostrar Guias');
+            
+        }
+
+        
+      });
+
+    }
 
 
 
